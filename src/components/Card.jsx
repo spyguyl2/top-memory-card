eddd2171b7366934e41eb89efc5f07e7a73bb6dd
Damon Haase <damon.haase@gmail.com>
2024-12-23 03:55:18 +0000

    Small additions before I leave work

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,9 @@
-const Card = () => {};
+const Card = ({ src, alt, onClick }) => {
+  return (
+    <div className="card" onClick={onClick}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+};
 
 export default Card;
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,7 @@
+import Card from "./Card";
+
+//import images here, i presume
+
 const GameBoard = () => {};
 
 export default GameBoard;
