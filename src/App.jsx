7d631948e4f8947eaeaca4a8f2e71467e527bd65
Damon Haase <damon.haase@gmail.com>
2024-12-25 23:30:59 +0000

    Begin organizing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { createClient } from "pexels";
 import Header from "./components/Header";
-
-const GIPHY_API_KEY = "Uuv7CSHnVgjoO73jFt5wI8rfn5b9gAob";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   const [score, setScore] = useState(0);
@@ -21,6 +19,7 @@ function App() {
   return (
     <>
       <Header score={score} bestScore={bestScore} />
+      <GameBoard />
     </>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,22 @@
 import Card from "./Card";
+import { useEffect } from "react";
 
-//import images here, i presume
+const GIPHY_API_KEY = "Uuv7CSHnVgjoO73jFt5wI8rfn5b9gAob";
+const FETCH_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
 
-const GameBoard = () => {};
+const GameBoard = () => {
+  useEffect(() => {
+    const getGifs = async () => {
+      const response = await fetch(FETCH_URL);
+      const gifData = await response.json();
+    };
+  }, []);
+
+  return (
+    <>
+      <img src="" alt="" />
+    </>
+  );
+};
 
 export default GameBoard;
