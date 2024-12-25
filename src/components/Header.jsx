9491201024c8196499bdd29df5679d799377f3d1
Damon Haase <damon.haase@gmail.com>
2024-12-25 20:57:39 +0000

    Give credit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = ({ score, bestScore }) => {
       <h1>
         How's <em>your</em> memory?!
       </h1>
+      <a href="https://www.pexels.com">Photos provided by Pexels</a>
       <div id="scoreBoard">
         <p>Score: {score}</p>
         <p>Best Score: {bestScore}</p>
