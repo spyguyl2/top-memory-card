f1ea889f2942cdffeb0b4e1ed8cee873edb627f9
Damon Haase <damon.haase@gmail.com>
2024-12-25 23:30:37 +0000

    Credit GIPHY

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = ({ score, bestScore }) => {
       <h1>
         How's <em>your</em> memory?!
       </h1>
+      <a href="https://giphy.com/">Powered by GIPHY!</a>
       <div id="scoreBoard">
         <p>Score: {score}</p>
         <p>Best Score: {bestScore}</p>
