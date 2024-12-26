e01ea9ec1209e9fb2264db914b8e21ffe241bd46
Damon Haase <damon.haase@gmail.com>
2024-12-26 14:03:34 -0500

    Attempting to make images appear

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,24 @@
 import Card from "./Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const GIPHY_API_KEY = "Uuv7CSHnVgjoO73jFt5wI8rfn5b9gAob";
 const FETCH_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
 
 const GameBoard = () => {
+  const [gifs, setGifs] = useState([]);
+
   useEffect(() => {
-    const getGifs = async () => {
+    async function getGifsArray() {
       const response = await fetch(FETCH_URL);
       const gifData = await response.json();
-    };
+      return gifData.data;
+    }
+    getGifsArray().then((data) => setGifs(data));
   }, []);
 
   return (
     <>
-      <img src="" alt="" />
+      <img src={gifs[0].src} alt="" />
     </>
   );
 };
