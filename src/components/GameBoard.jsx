5d6d0f6c45cb05013e107ef1d216bed715c83023
Damon Haase <damon.haase@gmail.com>
2024-12-26 22:17:49 +0000

    Fix api call

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,31 @@
 import Card from "./Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const GIPHY_API_KEY = "Uuv7CSHnVgjoO73jFt5wI8rfn5b9gAob";
 const FETCH_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
 
 const GameBoard = () => {
+  const [gifs, setGifs] = useState(null);
+  const [haveGifs, setHaveGifs] = useState(false);
+
   useEffect(() => {
     const getGifs = async () => {
-      const response = await fetch(FETCH_URL);
+      const response = await fetch(FETCH_URL, { mode: "cors" });
       const gifData = await response.json();
+      setGifs(gifData.data);
+      setHaveGifs(true);
     };
+    getGifs();
+    console.log(gifs);
   }, []);
 
   return (
     <>
-      <img src="" alt="" />
+      {!haveGifs ? (
+        <p>Loading gifs...</p>
+      ) : (
+        <img src={gifs[0].images.original.url} alt="" />
+      )}
     </>
   );
 };
